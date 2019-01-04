b51b7f9b26ae7aa7010c6331cae2a8e1b581c5e7
Brandon Friedenstein <Bfriedenstein@gmail.com>
2019-01-04 15:49:37 +0200

    [Libs] Added common libs

---
+++ libs/common/common.h
@@ -0,0 +1,21 @@
+/*
+ * common.h
+ *
+ *  Created on: 03 Jan 2019
+ *      Author: brandon
+ */
+
+#ifndef COMMON_COMMON_H_
+#define COMMON_COMMON_H_
+
+/* Application exit codes */
+#define EXIT_NORMAL 0
+#define EXIT_BAD_ARGS 1
+
+/* Audio constants */
+#define SAMPLE_RATE 44100
+#define BITDEPTH 16
+#define MIN_AMP -32768
+#define MAX_AMP 32767
+#define PI 3.14159265
+#endif /* COMMON_COMMON_H_ */
