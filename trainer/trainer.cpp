88a22bf4386b7b244335862088671d1000656fb4
Brandon Friedenstein <Bfriedenstein@gmail.com>
2019-01-05 14:15:07 +0200

    [Trainer] Added trainer shell

---
+++ trainer/trainer.cpp
@@ -0,0 +1,10 @@
+/*
+ * trainer.cpp
+ *
+ *  Created on: 04 Jan 2019
+ *      Author: brandon
+ */
+
+
+
+#include "trainer.h"
---
+++ trainer/trainer.h
@@ -0,0 +1,15 @@
+/*
+ * trainer.h
+ *
+ *  Created on: 04 Jan 2019
+ *      Author: brandon
+ */
+
+#ifndef TRAINER_TRAINER_H_
+#define TRAINER_TRAINER_H_
+
+
+
+
+
+#endif /* TRAINER_TRAINER_H_ */
